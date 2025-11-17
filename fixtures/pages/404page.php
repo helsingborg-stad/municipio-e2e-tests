57b0674bc862fcb23f6c5c047756648b06a18f06
Thor Brink <thor.brink@helsingborg.se>
2025-11-17 11:09:18 +0100

    fix: allow 404

---
+++ fixtures/pages/404page.php
@@ -0,0 +1,20 @@
+<?php header('Access-Control-Allow-Origin: http://localhost:8000'); ?>
+<?php header('Strict-Transport-Security: max-age=31536000;'); ?>
+<?php header('Content-Security-Policy: default-src \'self\';'); ?>
+<?php header('HTTP/1.1 404 Not Found'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page 4</title>
+</head>
+
+<body>
+    <h1>404 Page</h1>
+    <p>Testing that response code 404 is allowed.</p>
+</body>
+
+</html>
